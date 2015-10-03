af42b423cdcd85294f631c5e3ecb4eeb37f8d535
Kiss Dániel <kissdani89@gmail.com>
2015-10-03 02:49:14 +0200

    Müködik

---
+++ WCF_Echo/EchoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Echo
+{
+    [ServiceContract]
+    class EchoService
+    {
+
+        [OperationContract]
+        void Echo(string line)
+        {
+
+            Console.WriteLine(line);
+        }
+
+    }
+}
--- WCF_Echo/Program.cs
+++ WCF_Echo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 
 namespace WCF_Echo
@@ -9,6 +11,23 @@ namespace WCF_Echo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Echo..");
+
+            Uri baseAddress = new Uri("http://localhost:8000/echo");
+            using (ServiceHost selfHost = new ServiceHost(typeof(EchoService), baseAddress))
+            {
+                //selfHost.AddServiceEndpoint(typeof(EchoService), new BasicHttpBinding(), "EchoService");
+                selfHost.AddServiceEndpoint(typeof(EchoService), new WSHttpBinding(), "EchoService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.ReadLine();
+
+            }
         }
     }
 }
