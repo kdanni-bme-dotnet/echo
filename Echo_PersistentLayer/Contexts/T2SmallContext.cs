a4bc0dab66489809578ef57114969261b41d2bc8
danielkiss <kissdani89@gmail.com>
2015-10-14 17:48:50 +0200

    context

---
+++ Echo_PersistentLayer/Contexts/T2SmallContext.cs
@@ -0,0 +1,31 @@
+﻿using Echo_PersistentLayer.Entities.Debug;
+using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo_PersistentLayer.Contexts
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class T2SmallContext : DbContext
+    {
+
+        public DbSet<DebugEntity> DebugEntities{ get; set; }
+
+        public T2SmallContext(String connectionString)
+            : base(connectionString)
+        {
+
+
+        }
+
+        public T2SmallContext()
+        {
+
+
+        }
+    }
+}
