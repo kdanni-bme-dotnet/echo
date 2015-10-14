253ab02c99e0305791204524bed95185f008b273
danielkiss <kissdani89@gmail.com>
2015-10-14 12:58:32 +0200

    Initial commit EF

---
+++ Echo_PersistentLayer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Echo_PersistentLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
