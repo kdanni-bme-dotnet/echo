660bc9ea7e5043be1c2687ee6082937d3c502114
danielkiss <kissdani89@gmail.com>
2015-10-14 12:29:43 +0200

    commit

---
+++ WCF_Echo_Client/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WCF_Echo_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string line = textBox.Text;
+
+            using (EchoClient.EchoServiceClient ec = new EchoClient.EchoServiceClient()) {
+                ec.Echo(line);
+            }
+        }
+    }
+}
