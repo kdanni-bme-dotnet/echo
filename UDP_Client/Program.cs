ba2f2f35cfade4fa70f06428c1d286f45796ec6e
Kiss Dániel <kissdani89@gmail.com>
2015-11-07 00:03:40 +0100

    comment

---
+++ UDP_Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace UDP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ UDP_Host/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+                       
+        }
+    }
+}
