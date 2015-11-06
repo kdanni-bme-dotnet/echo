6a76d3926e304cd181a20e8a85e01fcb672b2404
Kiss Dániel <kissdani89@gmail.com>
2015-11-06 22:54:41 +0100

    udp service

---
+++ UDP_Service/IUdpService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace UDP_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUdpService" in both code and config file together.
+    [ServiceContract]
+    public interface IUdpService
+    {
+        [OperationContract(IsOneWay = true)]
+        void DoWork();
+
+        [OperationContract(IsOneWay = true)]
+        void ProcessDatagram(Datagram datagram);
+    }
+
+    [DataContract]
+    public class Datagram
+    {
+
+        [DataMember]
+        public string Metadata { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Message { get; set; }
+
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+
+    }
+}
---
+++ UDP_Service/UdpService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace UDP_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UdpService" in both code and config file together.
+    public class UdpService : IUdpService
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("Working...");
+        }
+
+        public void ProcessDatagram(Datagram datagram)
+        {
+            Console.WriteLine("Datagram: " + datagram);
+        }
+    }
+}
